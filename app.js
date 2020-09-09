4bd1a50633b9e3a55ee16b02f5530d69757496eb
casperfibaek <casperfibaek@gmail.com>
2020-09-09 16:05:30 +0200

    functions test - solving cors

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
@@ -20,7 +19,6 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
 app.use(cookieParser());
 app.use(
     bodyParser.urlencoded({
@@ -29,6 +27,7 @@ app.use(
 );
 app.use((req, res, next) => {
     res.setHeader('Cache-Control', `public, max-age=${String(defaults.cache)}`);
+    res.setHeader('Access-Control-Allow-Origin', '*');
     next();
 });
 
