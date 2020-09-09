a3abbaca0a7312c7c442c4afc69743ae1dd75e84
casperfibaek <casperfibaek@gmail.com>
2020-09-09 15:38:57 +0200

    cors issues, renamed folder.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
@@ -6,7 +7,7 @@ const compression = require('compression');
 const path = require('path');
 
 // Custom routes
-const routesApi = require('./api');
+const routesApi = require('./api_routes');
 
 // Variables
 const defaults = {
@@ -19,6 +20,7 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 app.use(cookieParser());
 app.use(
     bodyParser.urlencoded({
--- excel_interface/taskpane/taskpane.js
+++ excel_interface/taskpane/taskpane.js
@@ -15,7 +15,7 @@ Office.onReady((info) => {
 
     // Determine if the user's version of Office supports all the Office.js APIs that are used in the tutorial.
     if (!Office.context.requirements.isSetSupported('ExcelApi', '1.7')) {
-        console.log('Sorry. The tutorial add-in uses Excel.js APIs that are not available in your version of Office.');
+        console.log('Sorry. The add-in uses Excel.js APIs that are not available in your version of Office.');
     }
 
     // Assign event handlers and other initialization logic.
